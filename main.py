e97e9158b0ebe021245edc54003badf2128ebd8c
Vladimir <volodyaklever@gmail.com>
2024-12-31 19:00:54 +0300

    tests_12_2.py
    31.12.2024 Домашнее задание по теме "Методы Юнит-тестирования"

--- main.py
+++ main.py
@@ -1,16 +1,42 @@
-# tests_12_1.py
-# 29.12.2024 Задача "Проверка на выносливость"
+# tests_12_2.py
+# 30.12.2024 Задача "Проверка на выносливость"
 
 class Runner:
-    def __init__(self, name):
+    def __init__(self, name, speed=5):
         self.name = name
         self.distance = 0
+        self.speed = speed
 
     def run(self):
-        self.distance += 10
+        self.distance += self.speed * 2
 
     def walk(self):
-        self.distance += 5
+        self.distance += self.speed
 
     def __str__(self):
         return self.name
+
+    def __eq__(self, other):
+        if isinstance(other, str):
+            return self.name == other
+        elif isinstance(other, Runner):
+            return self.name == other.name
+
+
+class Tournament:
+    def __init__(self, distance, *participants):
+        self.full_distance = distance
+        self.participants = list(participants)
+
+    def start(self):
+        finishers = {}
+        place = 1
+        while self.participants:
+            for participant in self.participants:
+                participant.run()
+                if participant.distance >= self.full_distance:
+                    finishers[place] = participant
+                    place += 1
+                    self.participants.remove(participant)
+
+        return finishers
--- test_main.py
+++ test_main.py
@@ -1,27 +1,41 @@
 import unittest
-from main import Runner  # импортируем класс Runner из файла main
+from main import Runner, Tournament  # импортируем класс Runner из файла main
 
-class RunnerTest(unittest.TestCase):
+class TournamentTest(unittest.TestCase):
 
-    def test_walk(self):
-        runner = Runner("test walk")
-        for i in range(10):
-            runner.walk()
-        self.assertEqual(runner.distance, 50) # методом assertEqual сравните distance со значением 50
+    @classmethod
+    def setUpClass(cls):
+        cls.all_results = {} # словарь в который сохраняются результаты всех тестов.
 
-    def test_run(self):
-        runner = Runner("test run")
-        for i in range(10):
-            runner.run()
-        self.assertEqual(runner.distance, 100) # методом assertEqual сравните distance со значением 100
+    def setUp(self):
+        self.usain = Runner("Усэйн", speed=10) # Бегун по имени Усэйн, со скоростью 10.
+        self.andrey = Runner("Андрей", speed=9) # Бегун по имени Андрей, со скоростью 9.
+        self.nick = Runner("Ник", speed=3) # Бегун по имени Ник, со скоростью 3.
 
-    def test_challenge(self):
-        runner1 = Runner("runner1")
-        runner2 = Runner("runner2")
-        for i in range(10):
-            runner1.run()
-            runner2.walk()
-        self.assertNotEqual(runner1.distance, runner2.distance) # расстояния не равны после 10 пробежек и 10 прогулок
+    @classmethod
+    def tearDownClass(cls): # метод, где выводятся all_results по очереди в столбец.
+        print("\nРезультаты:")
+        for test_name, result in cls.all_results.items():
+            print(f"{test_name} - {result}")
+
+    def test_usain_nick(self):
+        tournament = Tournament(90, self.usain, self.nick)
+        results = tournament.start()
+        TournamentTest.all_results["Забег Усэйн и Ник"] = {place: str(runner) for place, runner in results.items()}
+        self.assertTrue(results[max(results.keys())] == "Ник")
+
+    def test_andrey_nick(self):
+        tournament = Tournament(90, self.andrey, self.nick)
+        results = tournament.start()
+        TournamentTest.all_results["Забег Андрей и Ник"] = {place: str(runner) for place, runner in results.items()}
+        self.assertTrue(results[max(results.keys())] == "Ник")
+
+    def test_usain_andrey_nick(self):
+        tournament = Tournament(90, self.usain, self.andrey, self.nick)
+        results = tournament.start()
+        TournamentTest.all_results["Забег Усэйн, Андрей и Ник"] = {place: str(runner) for place, runner in results.items()}
+        self.assertTrue(results[max(results.keys())] == "Ник")
+
 
 if __name__ == "__main__":
     unittest.main()
